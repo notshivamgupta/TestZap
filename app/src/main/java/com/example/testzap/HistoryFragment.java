cc1ee76f7e184b5b0b6445c67ea5698d35a0a13d
Shivam <testzapofficial@gmail.com>
2020-10-04 16:02:17 +0530

    STORE DATA IN FIRESTORE

--- app/src/main/java/com/example/testzap/HistoryFragment.java
+++ app/src/main/java/com/example/testzap/HistoryFragment.java
@@ -1,25 +1,28 @@
 package com.example.testzap;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class HistoryFragment extends Fragment {
 
-
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("History");
     public HistoryFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_history, container, false);
+        return view;
+
     }
 }
---
+++ app/src/main/java/com/example/testzap/HistoryModel.java
@@ -0,0 +1,58 @@
+package com.example.testzap;
+
+public class HistoryModel {
+
+    String sub_name, sub_set;
+    int correct, incorrect, time_taken;
+
+    public HistoryModel() {
+    }
+
+    public HistoryModel(String sub_name, String sub_set, int correct, int incorrect, int time_taken) {
+        this.sub_name = sub_name;
+        this.sub_set = sub_set;
+        this.correct = correct;
+        this.incorrect = incorrect;
+        this.time_taken = time_taken;
+    }
+
+    public String getSub_name() {
+        return sub_name;
+    }
+
+    public void setSub_name(String sub_name) {
+        this.sub_name = sub_name;
+    }
+
+    public String getSub_set() {
+        return sub_set;
+    }
+
+    public void setSub_set(String sub_set) {
+        this.sub_set = sub_set;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
+
+    public int getIncorrect() {
+        return incorrect;
+    }
+
+    public void setIncorrect(int incorrect) {
+        this.incorrect = incorrect;
+    }
+
+    public int getTime_taken() {
+        return time_taken;
+    }
+
+    public void setTime_taken(int time_taken) {
+        this.time_taken = time_taken;
+    }
+}
