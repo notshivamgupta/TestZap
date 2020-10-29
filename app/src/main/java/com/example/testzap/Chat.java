9b4250c284003b0c5c51d4602bf3161ab6a3a8e9
planetarpit <orionarpit@users.noreply.github.com>
2020-10-29 18:53:03 +0530

    layout

--- app/src/main/java/com/example/testzap/Chat.java
+++ app/src/main/java/com/example/testzap/Chat.java
@@ -10,6 +10,7 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
